d821796489b1b7fd0fbc6bedc592cf6aeb4ca16a
Tanisha <tanisharohilla55@gmail.com>
2024-03-17 08:36:36 +0530

    Some files added

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,25 @@
 const express= require("express");
 const dotenv= require("dotenv");
 const { chats }=require("./data/data");
-
-const app= express();
+const connectDB=require("./config/db");
+const colors=require("colors");
+const userRoutes=require("./routes/userRoutes");
+const {notFound, errorHandler}=require("./middleware/errorMiddleware");
 dotenv.config();
+
+connectDB();
+const app= express();
+
+app.use(express.json()); //to accept JSON data
 app.get("/", (req,res) => {
     res.send("API is Running");
 });
 
-app.get("/api/chat", (req,res)=>{
-    res.send(chats);
-});
+app.use('/api/user', userRoutes);
 
-app.get("/api/chat/:id", (req,res) => {
-    //console.log(req.params.id);n
-    
-    const singleChat= chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
-});
+app.use(notFound);
+app.use(errorHandler);
 
-// app.get("/api/chat/:id", (req,res)=>{
-//     console.log(chats);
-// })
 
 const PORT=process.env.PORT || 5000;
 app.listen(5000, console.log(`Server Started on PORT ${PORT}`));
