d18d76e6274bd3a31a68ba9cbce1eb22db51237a
Tanisha <tanisharohilla55@gmail.com>
2024-04-04 23:14:10 +0530

    CHanges made

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const { registerUser }= require("../controllers/userControllers");
  const {
-//   registerUser,
+   registerUser,
    authUser,
-//   allUsers,
+   allUsers,
  } = require("../controllers/userControllers");
-// const { protect } = require("../middleware/authMiddleware");
+  const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-//router.route("/").get(protect, allUsers);
+router.route("/").get(protect, allUsers);
 router.route("/").post(registerUser);
 router.post("/login", authUser);
 
